30a33d03c7965008c3ddb622662b2d3f84cd787a
Eric Siber <eric.siber@gmail.com>
2017-10-05 11:24:54 +0200

    Etape 1

---
+++ src/main/java/co/simplon/threads/Main.java
@@ -0,0 +1,10 @@
+package co.simplon.threads;
+
+public class Main {
+	public static void main(String[] args) throws InterruptedException {
+		while (true) {
+			System.out.println("Coucou");
+			Thread.sleep(1000);
+		}
+	}
+}
