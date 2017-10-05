f0f47aeaec79fd59693fc169dab48831d664cf4d
Eric Siber <eric.siber@gmail.com>
2017-10-05 11:35:29 +0200

    Etape 2 ok mais avec du bricolage

--- src/main/java/co/simplon/threads/Main.java
+++ src/main/java/co/simplon/threads/Main.java
@@ -2,15 +2,37 @@ package co.simplon.threads;
 
 public class Main {
 	public static void main(String[] args) throws InterruptedException {
+		long duree = 0;
+		long temporisation = 500l;
+		long intervalleCoucou = 1000l;
+		long intervalleFaim = 1500l;
+
 		while (true) {
-			System.out.println("Coucou");
-			Thread.sleep(1000);
+			afficherCoucouSelonDureeEcoulee(duree, intervalleCoucou);
+			afficherFaimSelonDureeEcoulee(duree, intervalleFaim);
+			duree = dormirEtDecompter(temporisation, duree);
 		}
-		
-		// Eclipse dit "Unreachable code"
-		while (true) {
+
+	}
+
+	private static long dormirEtDecompter(long temporisation, long duree)
+			throws InterruptedException {
+		Thread.sleep(temporisation);
+		duree += temporisation;
+		return duree;
+	}
+
+	private static void afficherFaimSelonDureeEcoulee(long duree,
+			long intervalleFaim) {
+		if (duree % intervalleFaim == 0) {
 			System.out.println("J'ai faim");
-			Thread.sleep(1500);
+		}
+	}
+
+	private static void afficherCoucouSelonDureeEcoulee(long duree,
+			long intervalleCoucou) {
+		if (duree % intervalleCoucou == 0) {
+			System.out.println("Coucou");
 		}
 	}
 }
